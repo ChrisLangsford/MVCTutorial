6404610abcf116a58fef2f155a8f2a448ec1a239
Christopher Langsford <langz020@gmail.com>
2014-02-06 11:39:35 +0200

    Movie controller still to be added

--- MvcW3Tutorial/MvcW3Tutorial/Controllers/HomeController.cs
+++ MvcW3Tutorial/MvcW3Tutorial/Controllers/HomeController.cs
@@ -4,29 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcW3Tutorial.Controllers
+namespace MvcDemo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
+        { return View(); }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        { return View(); }
     }
 }
